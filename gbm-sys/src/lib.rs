b6905606d3b01ebc779f9bb21750bfd26388686d
K <kayo@illumium.org>
2021-04-20 23:48:42 +0500

    Added workaround for bindgen tests checking

--- gbm-sys/src/lib.rs
+++ gbm-sys/src/lib.rs
@@ -1,4 +1,8 @@
 #![allow(non_camel_case_types, non_upper_case_globals)]
+// Allowed this because some bindgen tests looks like
+// it tries to dereference null pointers but actually
+// it is not so.
+#![cfg_attr(test, allow(deref_nullptr))]
 
 extern crate libc;
 
